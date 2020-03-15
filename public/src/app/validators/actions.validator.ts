895df3756084c21226c86f37bb98efee67be3731
Michael Gira <michael@gira.dev>
2020-03-15 11:47:47 -0500

    Fix actions validation

--- public/src/app/validators/actions.validator.ts
+++ public/src/app/validators/actions.validator.ts
@@ -5,14 +5,18 @@ import { SerializedAction, ActionThenType } from '../../model/actions';
 export function ValidateActions(control: AbstractControl) {
 	// If all `purpose` fields are empty, then criteria as a whole is invalid
 	if (control.value.every(ensureActionParameters)) {
-		return { validActions: true };
+		return null;
 	}
-	return null;
+	return { validActions: true };
 }
 
+/**
+ * Returns true if the provided (serialized) action is valid
+ * @param action Serialized action
+ */
 function ensureActionParameters(action: SerializedAction) {
 	if (action.thenType === ActionThenType.ADD_TO_PLAYLIST && !action.thenId) {
-		return { actionThenId: true };
+		return false;
 	}
-	return null;
+	return true;
 }
