1dc1a7c386e3aa4ac5b1288f2b59491b1fe8a8c5
Michael Gira <michael@gira.dev>
2020-03-18 13:09:15 -0500

    Add switch form capabilities

--- public/src/app/switch-test/switch-test.component.ts
+++ public/src/app/switch-test/switch-test.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SwitchTestComponent implements OnInit {
 
+	value1: boolean = null;
+	value2 = true;
+
 	constructor() { }
 
 	ngOnInit() {
--- public/src/app/switch/switch.component.ts
+++ public/src/app/switch/switch.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 
 @Component({
 	selector: 'app-switch',
@@ -6,6 +6,15 @@ import { Component, OnInit } from '@angular/core';
 	styleUrls: ['./switch.component.scss']
 })
 export class SwitchComponent implements OnInit {
+
+	static counter = 0;
+
+	// Label for both the `id` and `for` attribute of the checkbox input and label, respectively.
+	// Must be unique for every switch on the page or else only the first one will work!
+	@Input() label = `checky-${SwitchComponent.counter++}`;
+
+	@Input() value: boolean = null;
+	@Output() valueChange = new EventEmitter<boolean>();
 
 	constructor() { }
 
